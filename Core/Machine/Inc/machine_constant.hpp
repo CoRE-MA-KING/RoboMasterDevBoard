703e9504f4ad9f5c54b74a9661d09493e486ec76
mmaakkyyii <kyoro3141592@gmail.com>
2024-01-23 01:09:07 +0900

    定数定義ファイルを追加

---
+++ Core/Machine/Inc/machine_constant.hpp
@@ -0,0 +1,16 @@
+/*
+ * machine_constant.hpp
+ *
+ *  Created on: Jan 22, 2024
+ *      Author: mmaakkyyii
+ */
+
+#ifndef MACHINE_CONSTANT_HPP_
+#define MACHINE_CONSTANT_HPP_
+
+const float wheel_r_mm = 152;
+const float machine_width_mm = 630;
+const float machine_length_mm = 540;
+
+const float control_period_ms = 1.0;
+#endif /* MACHINE_CONSTANT_HPP_ */
